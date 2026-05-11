9ebb257b815cad68974b42ca3de7bec7e2d97023
kaddour-youcef <kaddouryoucef18@gmail.com>
2026-05-11 20:03:09 +0200

    icons

--- lib/source.ts
+++ lib/source.ts
@@ -1,12 +1,13 @@
 import { docs } from 'collections/server';
 import { loader } from 'fumadocs-core/source';
+import { lucideIconsPlugin } from 'fumadocs-core/source/plugins/lucide-icons';
 import { docsContentRoute, docsImageRoute, docsRoute } from './shared';
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: docsRoute,
   source: docs.toFumadocsSource(),
-  plugins: [],
+  plugins: [lucideIconsPlugin()],
 });
 
 export function getPageImage(page: (typeof source)['$inferPage']) {
